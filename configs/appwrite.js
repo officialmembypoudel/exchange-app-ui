de05737ef6db55be2a63b7429fd5aedf3440b907
officialmembypoudel <96823214+officialmembypoudel@users.noreply.github.com>
2023-10-15 17:34:53 +0545

    Update appwrite.js

--- configs/appwrite.js
+++ configs/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client();
-client.setEndpoint("https://nepalinfo.itsoch.com/v1").setProject("madhyayuga");
+client.setEndpoint("api url").setProject("madhyayuga");
 
 export const account = new Account(client);
 
